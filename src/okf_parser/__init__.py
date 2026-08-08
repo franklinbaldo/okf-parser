3bce90d2fca8e3e33dc662667d4d320809ae71a7
Franklin Silveira Baldo <franklinbaldo@gmail.com>
2026-08-07 23:26:21 -0400

    feat: export typed relation owner

--- src/okf_parser/__init__.py
+++ src/okf_parser/__init__.py
@@ -2,10 +2,12 @@
 
 from okf_parser.bundle import Bundle, load_bundle, validate_path
 from okf_parser.models import Severity, ValidationReport, Violation
+from okf_parser.typed_relations import TypedRelations
 
 __all__ = [
     "Bundle",
     "Severity",
+    "TypedRelations",
     "ValidationReport",
     "Violation",
     "load_bundle",
