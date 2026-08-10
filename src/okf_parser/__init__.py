6cee08384e547e2d606b40f830d11f9142d127e0
Franklin Silveira Baldo <franklinbaldo@gmail.com>
2026-08-10 08:25:11 -0400

    feat(git): export commit message APIs

--- src/okf_parser/__init__.py
+++ src/okf_parser/__init__.py
@@ -2,6 +2,13 @@
 
 from okf_parser.bundle import Bundle, load_bundle, validate_path
 from okf_parser.edit import EditError, preview_concept_edit, write_concept_edit
+from okf_parser.git_commit import (
+    GitCommitMessage,
+    GitCommitMessageError,
+    format_git_commit_message,
+    parse_git_commit_message,
+    validate_git_commit_message,
+)
 from okf_parser.ingestion import DocumentEnvelope, IngestionCapability, ingest_documents
 from okf_parser.models import Severity, ValidationReport, Violation
 from okf_parser.typed_relations import TypedRelations
@@ -10,14 +17,19 @@ __all__ = [
     "Bundle",
     "DocumentEnvelope",
     "EditError",
+    "GitCommitMessage",
+    "GitCommitMessageError",
     "IngestionCapability",
     "Severity",
     "TypedRelations",
     "ValidationReport",
     "Violation",
+    "format_git_commit_message",
     "ingest_documents",
     "load_bundle",
+    "parse_git_commit_message",
     "preview_concept_edit",
+    "validate_git_commit_message",
     "validate_path",
     "write_concept_edit",
 ]
