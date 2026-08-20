7f041e175db59cf532a5afd0775f687bfb1cc93b
Franklin Silveira Baldo <franklinbaldo@gmail.com>
2026-08-20 08:29:03 -0400

    feat: expose concept relation helpers

--- src/okf_parser/__init__.py
+++ src/okf_parser/__init__.py
@@ -1,6 +1,7 @@
 """Relational inspection and validation for Open Knowledge Format bundles."""
 
 from okf_parser.bundle import Bundle, validate_path
+from okf_parser.concepts import ConceptView, concept, resolve_relations
 from okf_parser.edit import EditError, preview_concept_edit, write_concept_edit
 from okf_parser.engine import load_bundle
 from okf_parser.graphql_adapter import (
@@ -17,6 +18,7 @@ from okf_parser.typed_relations import TypedRelations
 
 __all__ = [
     "Bundle",
+    "ConceptView",
     "DocumentEnvelope",
     "EditError",
     "GraphQLAdapterUnavailableError",
@@ -29,10 +31,12 @@ __all__ = [
     "ValidationReport",
     "Violation",
     "build_graphql_schema",
+    "concept",
     "export_graphql_sdl",
     "ingest_documents",
     "load_bundle",
     "preview_concept_edit",
+    "resolve_relations",
     "validate_path",
     "write_concept_edit",
 ]
